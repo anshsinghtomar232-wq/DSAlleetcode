394c68f009b554430bfda7cce3d9a7c73b4df71d
anshsinghtomar232-wq <anshsinghtomar232@gmail.com>
2026-07-25 20:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (42.87%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -10,30 +10,32 @@
  * };
  */
 class Solution {
-private:
+public:
+   pair<int,int> diameterfast(TreeNode*root){
+     if(root==NULL){
+        pair<int,int> p =make_pair(0,0);
+        return p;
+     }
 
+     pair<int,int> left=diameterfast(root->left);
+     pair<int,int> right=diameterfast(root->right);
 
-   int height(TreeNode*root){
-    if(root==NULL){
-        return 0;
-    }
+     int op1=left.first;
+     int op2=right.first;
+      int op3=left.second+right.second;
 
-    int left=height(root->left);
-    int right=height(root->right);
+      pair<int,int> ans;
+      ans.first=max(op1,max(op2,op3));;
+      ans.second=max(left.second,right.second)+1;
+      return ans;
 
-    return max(left,right)+1;
 
    }
-public:
+
+
+  
+
     int diameterOfBinaryTree(TreeNode* root) {
-
-         if(root==NULL){
-            return 0;
-         }
-        int op1=diameterOfBinaryTree(root->left);
-        int op2=diameterOfBinaryTree(root->right);
-        int op3=height(root->right)+height(root->left);
-
-        return max(op1,max(op2,op3));
+        return diameterfast(root).first;
     }
 };
