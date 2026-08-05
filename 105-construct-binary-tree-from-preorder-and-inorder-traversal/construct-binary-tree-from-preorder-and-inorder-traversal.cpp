8ba740ceed73bc232c6135b3759738a8ce180066
anshsinghtomar232-wq <anshsinghtomar232@gmail.com>
2026-08-05 14:55:14 +0530

    Time: 2 ms (77.72%) | Memory: 27.4 MB (69.24%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,32 +12,28 @@
 class Solution {
 public:
 
-void createMapping(vector<int> inorder, map<int,int> &nodetoindex,int n){
-    for(int i=0;i<n;i++){
-        nodetoindex[inorder[i]]=i;
-    }
-    
-  }
- TreeNode*solve(vector<int>&preorder,vector<int> &inorder, int&index,int inorderstart,int inorderend ,int n,map<int,int> &nodetoindex){
-    if(index>=n || inorderstart> inorderend){
+ TreeNode*solve(vector<int>&preorder,vector<int> &inorder, int&index,int inorderstart,int inorderend ,unordered_map<int,int> &nodetoindex){
+    if(index>=inorder.size()|| inorderstart> inorderend){
         return NULL;
     }
     int element=preorder[index++];
     TreeNode* root= new TreeNode(element);
     int position=nodetoindex[element];
 
-    root->left=solve(preorder, inorder,index,inorderstart,position-1,n,nodetoindex);
+    root->left=solve(preorder, inorder,index,inorderstart,position-1,nodetoindex);
 
-    root->right=solve(preorder,inorder,index,position+1,inorderend,n,nodetoindex);
+    root->right=solve(preorder,inorder,index,position+1,inorderend,nodetoindex);
     return root;
  }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int preorderindex=0;
         int n=inorder.size();
-        map<int,int> nodetoindex;
-        createMapping(inorder,nodetoindex,n);
+       unordered_map<int,int> nodetoindex;
+        for(int i=0;i<n;i++){
+            nodetoindex[inorder[i]]=i;
+        }
        
-       TreeNode*ans= solve(preorder,inorder,preorderindex,0,n-1,n,nodetoindex);
-          return ans;
+       return solve(preorder,inorder,preorderindex,0,n-1,nodetoindex);
+          
     }
 };
