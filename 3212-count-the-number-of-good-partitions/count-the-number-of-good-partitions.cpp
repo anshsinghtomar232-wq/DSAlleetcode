f23e23c3ae5b5ce4bbe322b48afeb0149fc7db87
anshsinghtomar232-wq <anshsinghtomar232@gmail.com>
2026-08-08 15:10:28 +0530

    Time: 114 ms (29.44%) | Memory: 138.6 MB (76.18%) - LeetSync

--- 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.cpp
+++ 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-
-     
     int numberOfGoodPartitions(vector<int>& nums) {
-      int n=nums.size() ;
-        int M=1e9+7;
-      unordered_map<int,int> last_index;
-      for(int i=0;i<n;i++){
-        last_index[nums[i]]=i;
-      }
-
-      int i=0;
-      int j=0;
-      int result=1;
-      while(i<n){
-        if( i>j){
-            result=(result*2) % M;
+        int n = nums.size();
+        int M = 1e9 + 7;
+        
+        unordered_map<int, int> last_index;
+        for (int i = 0; i < n; i++) {
+            last_index[nums[i]] = i;
         }
-        j=max(j,last_index[nums[i]]);
-        i++;
-      }
 
-      return result;
+        int result = 1;
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+        
+            if (i > j) {
+                result = (result * 2) % M;
+            }
+            j = max(j, last_index[nums[i]]);
+        }
+
+        return result;
     }
 };
